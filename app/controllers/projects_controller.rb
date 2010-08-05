5dac8404f5c8ab0e91c50128167516389a74816c
Marcus Swope <marcusswope@gmail.com>
2010-08-04 20:01:38 -0500

    changed menu to incorporate with header

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
-    @projects = Project.all
+    @projects = Project.find_all_by_archived false
 
     respond_to do |format|
       format.html # index.html.erb
